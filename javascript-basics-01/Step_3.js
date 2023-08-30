61512c907b5f7fe7f6cd891db6f77509d39dfdd6
Hassan Mourtada <hassanmourtada15>
2023-08-30 16:18:55 +0300

    Completed Step_3

---
+++ javascript-basics-01/Step_3.js
@@ -0,0 +1,6 @@
+let userName = prompt("Please enter your name:")
+if (userName !== null && userName !== "") {
+    alert("Hello, " + userName + "!");
+} else {
+    alert("You didn't provide a name.");
+}
